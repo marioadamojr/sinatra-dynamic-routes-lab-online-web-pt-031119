a3d29dcce23af57ed91377058f09d95bb688fe3c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-15 17:46:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num=params[:number].to_i
     @phr=params[:phrase]
-    @num.times { "#{@phr}"}
+    /@num.times { "#{@phr}"}
   end
 end
